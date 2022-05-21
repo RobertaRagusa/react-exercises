4604d2fab5fafbfe92ebd440fc36aa817583e7fa
Roberta Ragusa <roberta.rg.99@gmail.com>
2022-05-21 18:54:05 +0200

    lists-03

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,33 +3,43 @@ import React from "react";
 export class TodoList extends React.Component {
   state = {
     items: ["Red", "Yellow", "Orange"],
+    input: "",
   };
 
-  handleAddItem = () => {
-    const newItem = this.newItem.value;
+  componentDidMount = () => {
+    this.setState({
+      items: this.state.items.map((item) => <li>{item}</li>),
+    });
+  };
+
+  handleInput = (event) => {
+    const value = event.target.value;
 
     this.setState({
-      items: [...this.state.items, newItem],
+      input: value,
+    });
+  };
+
+  addItem = () => {
+    this.state.items.push(<li>{this.state.input}</li>);
+    this.setState({
+      input: "",
     });
   };
 
   render() {
-    const { items } = this.state;
     return (
       <div>
         <input
-          ref={(input) => (this.newItem = input)}
           type="text"
           placeholder="Item"
+          value={this.state.input}
+          onChange={this.handleInput}
         ></input>
-        <button type="submit" onClick={this.handleAddItem}>
+        <button type="submit" onClick={this.addItem}>
           Add Item
         </button>
-        <ul>
-          {items.map((item, index) => (
-            <li key={item + index}>{item}</li>
-          ))}
-        </ul>
+        <ul>{this.state.items}</ul>
       </div>
     );
   }
