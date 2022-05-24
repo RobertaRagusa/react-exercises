e43a9379cec87c4c34cb954237bf6b3a976bde58
Roberta Ragusa <roberta.rg.99@gmail.com>
2022-05-24 17:25:14 +0200

    useState-01

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
-export class ClickCounter extends React.Component {
+export function ClickCounter({ initialValue = 0 }) {
+  const [counter, setCounter] = useState(initialValue);
+
+  function handleCounterIncrement() {
+    setCounter((c) => c + 1);
+  }
+
+  return <button onClick={handleCounterIncrement}>Counter: {counter}</button>;
+}
+
+/*export class ClickCounter extends React.Component {
   state = { count: 0 };
 
   handleCounterIncrement() {
@@ -18,4 +28,4 @@ export class ClickCounter extends React.Component {
       </button>
     );
   }
-}
+}*/
