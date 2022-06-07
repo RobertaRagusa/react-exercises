96496db13d72543fbe6d1a4e333e3705d7d36c54
Roberta Ragusa <roberta.rg.99@gmail.com>
2022-06-07 15:24:37 +0200

    custom-hooks-03

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,18 +1,7 @@
-import { useEffect, useState } from "react";
+import { useGithubUser } from "./useGithubUser";
 
 export function GithubUser({ username }) {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch(`https://api.github.com/users/${username}`)
-      .then((response) => {
-        return response.json();
-      })
-      .then((json) => {
-        console.log(json);
-        setData(json);
-      });
-  }, [username]);
+  const { data } = useGithubUser(username);
 
   return <div>{data && <h1>{data.username}</h1>}</div>;
 }
