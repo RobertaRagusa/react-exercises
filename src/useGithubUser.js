1b6029f488e143dfd848b6d9e3328f5025e50032
Roberta Ragusa <roberta.rg.99@gmail.com>
2022-06-09 19:37:38 +0200

    swr-01

--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+/*import { useEffect, useState } from "react";
 
 export function useGithubUser(username) {
   const [data, setData] = useState(null);
@@ -27,4 +27,18 @@ export function useGithubUser(username) {
   }, [username]);
 
   return { data, error, loading };
+}*/
+
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((response) => response.json());
+
+export function useGithubUser() {
+  const { data, error } = useSWR(`https://api.github.com/users`, fetcher);
+
+  return {
+    users: data,
+    error,
+    isLoading: !data && !error,
+  };
 }
