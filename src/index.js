51b3731f26da4f639034cf7c3aa44287d3d7aaa0
Roberta Ragusa <roberta.rg.99@gmail.com>
2022-06-10 20:31:30 +0200

    redux-03

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 // import React from "react";
 // import ReactDOM from "react-dom";
 import "./index.css";
-import { incrementCounter } from "./state/CounterReducer";
+import {
+  decrementCounter,
+  incrementCounter,
+  reset,
+} from "./state/CounterReducer";
 import { store } from "./state/Store";
-import { addTodos } from "./state/TodosReducer";
+import { addTodos, editTodos, removeTodos } from "./state/TodosReducer";
 // import { Root } from "./Root";
 
 // ReactDOM.render(<Root />, document.getElementById("root"));
@@ -12,4 +16,9 @@ store.subscribe(() => {
 });
 
 store.dispatch(incrementCounter(5));
-store.dispatch(addTodos({ id: 1, title: "Title", complited: true }));
+store.dispatch(decrementCounter(2));
+store.dispatch(reset());
+store.dispatch(addTodos({ id: 1, title: "Title one", complited: true }));
+store.dispatch(addTodos({ id: 2, title: "Title two", complited: true }));
+store.dispatch(editTodos(2, { title: "Title two update" }));
+store.dispatch(removeTodos(1));
